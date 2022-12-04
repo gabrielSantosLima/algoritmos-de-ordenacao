3d5fe771d7253c671a91adb9fb9256a4d7b4754a
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2022-12-04 19:56:47 -0400

    add bucket sort tests

---
+++ README.md
@@ -0,0 +1,23 @@
+# Algoritmos de Ordenação - Merge Sort e Bucket Sort
+
+Repositório referente a atividade de Algoritmo e Estrutura de Dados II do curso de
+Sistemas de Informação da **UEA EST**.
+
+## Sumário
+
+- [Código Fonte](#código-fonte)
+- [Autores](#autores)
+
+## Código Fonte
+
+- [Código do algoritmo de Merge Sort](./merge-sort.c)
+- [Código do algoritmo de Bucket Sort](./bucket-sort.c)
+
+## Autores
+
+- Arthur Uguen
+- Débora Barros
+- Diogo Pena
+- Gabriel Dos Santos
+- Guilherme Tapajós
+- Melinne Diniz
--- bucket-sort.c
+++ bucket-sort.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <inttypes.h>
 
-#define MAX 10
 #define false 0
 #define true 1
 
@@ -25,7 +25,6 @@ ARRAY *newArray(int maxLength)
     newArray->data = (double *)malloc(sizeof(double) * maxLength);
     return newArray;
 }
-
 
 bool isEmpty(ARRAY *array)
 {
@@ -52,13 +51,16 @@ void printArray(ARRAY *array)
     if (isEmpty(array))
         return;
     for (int index = 0; index < array->lenght; index++)
-        printf("%.2f ", array->data[index]);
+        printf("%f ", array->data[index]);
 }
 
-ARRAY* concat(ARRAY* arrays[], int maxLength){
-    ARRAY* array = newArray(maxLength);
-    for(int index = 0; index < maxLength; index++){
-        for(int elementIndex = 0; elementIndex < arrays[index]->lenght; elementIndex++){
+ARRAY *concat(ARRAY *arrays[], int maxLength)
+{
+    ARRAY *array = newArray(maxLength);
+    for (int index = 0; index < maxLength; index++)
+    {
+        for (int elementIndex = 0; elementIndex < arrays[index]->lenght; elementIndex++)
+        {
             add(array, arrays[index]->data[elementIndex]);
         }
     }
@@ -70,50 +72,59 @@ ARRAY* concat(ARRAY* arrays[], int maxLength){
 typedef struct NODE
 {
     double value;
-    struct NODE* nextNode;
-}NODE;
+    struct NODE *nextNode;
+} NODE;
 
-typedef struct LIST{
+typedef struct LIST
+{
     int length;
-    NODE* data;
-}LIST;
+    NODE *data;
+} LIST;
 
-LIST* newList(){
-    LIST* newList  = (LIST*) malloc(sizeof(LIST));
+LIST *newList()
+{
+    LIST *newList = (LIST *)malloc(sizeof(LIST));
     newList->data = NULL;
     newList->length = 0;
     return newList;
 }
 
-NODE* createNode(double value){
-    NODE* newNode = (NODE*) malloc(sizeof(NODE));
+NODE *createNode(double value)
+{
+    NODE *newNode = (NODE *)malloc(sizeof(NODE));
     newNode->value = value;
     newNode->nextNode = NULL;
     return newNode;
 }
 
-void push(LIST* list, double value){
-    if(list == NULL) return;
-    if(list->length == 0){
+void push(LIST *list, double value)
+{
+    if (list == NULL)
+        return;
+    if (list->length == 0)
+    {
         list->data = createNode(value);
         list->length++;
         return;
     }
-    NODE* auxNode = list->data;
-    while(auxNode->nextNode != NULL)
+    NODE *auxNode = list->data;
+    while (auxNode->nextNode != NULL)
         auxNode = auxNode->nextNode;
     auxNode->nextNode = createNode(value);
     list->length++;
 }
 
-void printList(LIST* list){
-    if(list == NULL || (list!=NULL && list->length == 0)){
+void printList(LIST *list)
+{
+    if (list == NULL || (list != NULL && list->length == 0))
+    {
         printf("Lista vazia");
         return;
     }
-    NODE* node = list->data;
-    while(node != NULL){
-        printf("%.2f ", node->value);
+    NODE *node = list->data;
+    while (node != NULL)
+    {
+        printf("%f ", node->value);
         node = node->nextNode;
     }
 }
@@ -121,19 +132,14 @@ void printList(LIST* list){
 // =====================================================
 
 /* ==================== Bucket Sort ==================== */
-void printBuckets(LIST* buckets[], int length){
-    for(int index = 0; index < length; index++){
-        printf("Bucket %d: ", index);
-        printList(buckets[index]);
-        printf("\n");
-    }
-}
-
-ARRAY* listToArray(LIST* list){
-    if(list == NULL) return NULL;
-    ARRAY* array = newArray(list->length);
-    NODE* auxNode = list->data;
-    while(auxNode != NULL){
+ARRAY *listToArray(LIST *list)
+{
+    if (list == NULL)
+        return NULL;
+    ARRAY *array = newArray(list->length);
+    NODE *auxNode = list->data;
+    while (auxNode != NULL)
+    {
         add(array, auxNode->value);
         auxNode = auxNode->nextNode;
     }
@@ -142,11 +148,14 @@ ARRAY* listToArray(LIST* list){
 
 void insertionSort(ARRAY *array)
 {
-    if(isEmpty(array) || (!isEmpty(array) && array->lenght == 1)) return;
-    for(int index = 1; index < array->lenght; index++){
+    if (isEmpty(array) || (!isEmpty(array) && array->lenght == 1))
+        return;
+    for (int index = 1; index < array->lenght; index++)
+    {
         double key = array->data[index];
         int i = index - 1;
-        while(i >= 0 && array->data[i] > key){
+        while (i >= 0 && array->data[i] > key)
+        {
             array->data[i + 1] = array->data[i];
             i = i - 1;
         }
@@ -157,41 +166,155 @@ void insertionSort(ARRAY *array)
 void bucketSort(ARRAY *array, int maxLength)
 {
     LIST *buckets[maxLength];
-    for(int index = 0; index < maxLength; index++){
+    for (int index = 0; index < maxLength; index++)
         buckets[index] = newList();
-    }
-    for(int index = 0; index < maxLength; index++){
+    for (int index = 0; index < maxLength; index++)
+    {
         int position = floor(maxLength * array->data[index]);
         push(buckets[position], array->data[index]);
     }
-    printBuckets(buckets, maxLength);
     ARRAY *bucketsAsArray[maxLength];
-    for(int index = 0; index < maxLength; index++){
-        ARRAY* bucketAsArray = listToArray(buckets[index]);
+    for (int index = 0; index < maxLength; index++)
+    {
+        ARRAY *bucketAsArray = listToArray(buckets[index]);
         insertionSort(bucketAsArray);
         bucketsAsArray[index] = bucketAsArray;
     }
-    ARRAY* sortedArray = concat(bucketsAsArray, maxLength);
-    for(int index = 0; index < sortedArray->lenght; index++){
+    ARRAY *sortedArray = concat(bucketsAsArray, maxLength);
+    for (int index = 0; index < sortedArray->lenght; index++)
         array->data[index] = sortedArray->data[index];
-    }
 }
 // =====================================================
 
-void test1000(){
-    ARRAY *array = newArray(1000);
-    srand(time(NULL));
-    for(int index = 0; index < 1000; index++){
-        double number = (rand() % 100);
-        number /= 100;
-        add(array, number);
+/* ==================== Utilidades ==================== */
+void printTitle(char *text)
+{
+    char *divisor = "==================";
+    printf("\n%s ", divisor);
+    printf(text);
+    printf(" %s\n", divisor);
+}
+
+void printTime(double seconds)
+{
+    printf("\nFinalizado em %.2f segundos.\n", seconds);
+}
+
+typedef clock_t timestamp;
+
+timestamp getTime()
+{
+    return clock();
+}
+
+double calcTime(timestamp start, timestamp end)
+{
+    return ((double)end - start) / CLOCKS_PER_SEC;
+}
+
+void countTimeOf(void (*fn)())
+{
+    timestamp start = getTime();
+    fn();
+    timestamp end = getTime();
+    printTime(calcTime(start, end));
+}
+
+// 0 -> 0.0
+// 11 (11/10 = 1) -> 0.11
+// 101 (101/10 = 10) -> 0.101
+// 1001 (1001/10 = 100) -> 0.1001
+// 10001 (10001/10 = 1000) -> 0.10001
+// 100001 (100001/10 = 10000) -> 0.100001
+double getDecimalUnits(double value, int decimal)
+{
+    if (value < 10)
+        return pow(10, decimal);
+    return getDecimalUnits(value / 10, decimal + 1);
+}
+// =====================================================
+
+/* ==================== Requisitos para o código ==================== */
+/* Codificação e avaliação com 100000 números:
+- Vetor totalmente ordenado decrescentemente;
+- Vetor totalmente ordenado crescentemente;
+- Primeira metade ordenada crescente, segunda metade ordenada decrescentemente;
+- Primeira metade ordenada decrescente, segunda metade ordenada crescentemente;
+- Elementos totalmente desordenados.
+*/
+
+void testDecrescentNumbers()
+{
+    ARRAY *array = newArray(100000);
+    for (int index = 100000; index > 0; index--)
+        add(array, index / getDecimalUnits(index, 1));
+    bucketSort(array, array->maxLength);
+    // printArray(array); // Remover este comentário para ver a saída
+}
+
+void testCrescentNumbers()
+{
+    ARRAY *array = newArray(100000);
+    for (int index = 1; index <= 100000; index++)
+        add(array, index / getDecimalUnits(index, 1));
+    bucketSort(array, array->maxLength);
+    // printArray(array); // Remover este comentário para ver a saída
+}
+
+void testCrescentDecrescentNumbers()
+{
+    ARRAY *array = newArray(100000);
+    for (int index = 1; index <= 50000; index++)
+        add(array, index / getDecimalUnits(index, 1));
+    for (int index = 100000; index >= 50001; index--)
+        add(array, index / getDecimalUnits(index, 1));
+    bucketSort(array, array->maxLength);
+    // printArray(array); // Remover este comentário para ver a saída
+}
+
+void testDecrescentCrescentNumbers()
+{
+    ARRAY *array = newArray(100000);
+    for (int index = 50000; index >= 1; index--)
+        add(array, index / getDecimalUnits(index, 1));
+    for (int index = 50001; index <= 100000; index++)
+        add(array, index / getDecimalUnits(index, 1));
+    bucketSort(array, array->maxLength);
+    // printArray(array); // Remover este comentário para ver a saída
+}
+
+void testRandomNumbers()
+{
+    ARRAY *array = newArray(100000);
+    for (int index = 0; index < 100000; index++)
+    {
+        uint64_t sortedNumber;
+        sortedNumber = rand();
+        sortedNumber = (sortedNumber << 32) | rand();
+        sortedNumber = (sortedNumber % (999999999 - 100000000)) + 100000000;
+
+        int number = sortedNumber % 100001;
+        add(array, number / getDecimalUnits(number, 1));
     }
-    bucketSort(array, 1000);
-    printArray(array);
+    bucketSort(array, array->maxLength);
+    // printArray(array) // Remover este comentário para ver a saída
 }
 
 int main()
 {
-    test1000();
+    printTitle("Números Decrescentes");
+    countTimeOf(testDecrescentNumbers);
+
+    printTitle("Números Crescentes");
+    countTimeOf(testCrescentNumbers);
+
+    printTitle("Números Crescente-Decrescente");
+    countTimeOf(testCrescentDecrescentNumbers);
+
+    printTitle("Números Decrescente-Crescente");
+    countTimeOf(testDecrescentCrescentNumbers);
+
+    printTitle("Números Aleatórios");
+    countTimeOf(testRandomNumbers);
     return 0;
 }
