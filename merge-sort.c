35466d93c30b678e6de3fc44e41250a834549c7f
artuguen28 <87493045+artuguen28@users.noreply.github.com>
2022-12-11 22:24:57 -0400

    merge-sort final code

--- merge-sort.c
+++ merge-sort.c
@@ -21,10 +21,10 @@ typedef int bool;
 
 // ==================== Array ====================
 typedef struct ARRAY
-{ 
-	int length;   // Tamanho atual do vetor
-	int size;     // Tamanho máximo do vetor
-	double *data; // Primeiro elemento do vetor
+{
+	int length; // Tamanho atual do vetor
+	int size;	// Tamanho máximo do vetor
+	int *data;	// Primeiro elemento do vetor
 
 } ARRAY;
 
@@ -34,15 +34,15 @@ ARRAY *new_array(int size)
 	ARRAY *narray = (ARRAY *)malloc(sizeof(ARRAY));
 	narray->length = 0;
 	narray->size = size;
-	narray->data = (double *)malloc(sizeof(double) * size); // Aloca um vetor de double de tamanho sizeof(double) * size
+	narray->data = (int *)malloc(sizeof(int) * size); // Aloca um vetor de int de tamanho sizeof(int) * size
 	return narray;
 }
 
 // Verifica se vetor está vazio
 bool is_empty(ARRAY *array)
 {
-    return array == NULL ||
-           (array != NULL && array->length == 0);
+	return array == NULL ||
+		   (array != NULL && array->length == 0);
 }
 
 // Verifica se vetor está cheio
@@ -53,7 +53,7 @@ bool is_full(ARRAY *array)
 }
 
 // Adiciona um elemento em um vetor
-void add(ARRAY *array, double value)
+void add(ARRAY *array, int value)
 {
 	if (is_full(array))
 		return;
@@ -79,48 +79,51 @@ void merge(ARRAY *array, int inicio, int meio, int fim)
 
 	ARRAY *aux1 = new_array(n1);
 	ARRAY *aux2 = new_array(n2);
-	//int aux1[n1], aux2[n2]; 
-	// criando os vetores auxiliares
- 
-	int i = 0; //percorrerá o vetor aux1
-	int j = 0; //percorrerá o vetor aux2
-	int k = inicio; //percorrerá o vetor original
- 
- 
+	// int aux1[n1], aux2[n2];
+	//  criando os vetores auxiliares
+
+	int i = 0;		// percorrerá o vetor aux1
+	int j = 0;		// percorrerá o vetor aux2
+	int k = inicio; // percorrerá o vetor original
+
 	// colocando cada metade do vetor original em seu vetor auxiliar correspondente
-	for(int i = 0; i < n1; i++){
-		aux1[i] = array[inicio+i];
+	for (int i = 0; i < n1; i++)
+	{
+		aux1->data[i] = array->data[inicio + i];
 	}
-	for(int j=0; j < n2; j++){
-		aux2[j] = array[meio+1 + j];
+	for (int j = 0; j < n2; j++)
+	{
+		aux2->data[j] = array->data[meio + 1 + j];
 	}
- 
- 
+
 	// Comparações serão feitas entre os vetores auxiliares e os menores elementos serão inseridos no vetor original
-	while(i < n1 && j < n2){
+	while (i < n1 && j < n2)
+	{
 		if (aux1->data[i] <= aux2->data[j])
 		{
 
 			array->data[k] = aux1->data[i];
 			i++;
 		}
-		else {
+		else
+		{
 			array->data[k] = aux2->data[j];
 			j++;
 		}
 		k++;
 	}
- 
- 
-	//ainda há elementos no vetor aux1
-	while(i < n1){
+
+	// ainda há elementos no vetor aux1
+	while (i < n1)
+	{
 		array->data[k] = aux1->data[i];
 		i++;
 		k++;
 	}
- 
-	//ainda há elementos no vetor aux2
-	while(j < n2){
+
+	// ainda há elementos no vetor aux2
+	while (j < n2)
+	{
 		array->data[k] = aux2->data[j];
 		j++;
 		k++;
@@ -132,14 +135,13 @@ void merge_sort(ARRAY *array, int inicio, int fim)
 	if (inicio < fim)
 	{
 		int meio = (inicio + fim) / 2;
-		merge_sort(array, inicio, meio);	 // ordena a primeira parte (início até o meio)
+		merge_sort(array, inicio, meio);  // ordena a primeira parte (início até o meio)
 		merge_sort(array, meio + 1, fim); // ordena a segunda parte (meio até o fim)
-		merge(array, inicio, meio, fim); // combina as duas partes intercalando-as de maneira ordenada
+		merge(array, inicio, meio, fim);  // combina as duas partes intercalando-as de maneira ordenada
 	}
 }
 
-
-void printVetor(ARRAY * array, int tam)
+void printVetor(ARRAY *array, int tam)
 {
 	for (int i = 0; i < tam; i++)
 	{
@@ -190,13 +192,6 @@ void count_time(void (*fn)())
 	print_time(calc_time(start, end)); // Exibe a quantidade de tempo calculada
 }
 
-// Retorna quantidade de casas a serem retiradas tal que 'value' seja um valor x, onde 0 <= x <= 1
-double get_decimal(double value, int decimal)
-{
-	if (value < 10)
-		return pow(10, decimal);
-	return get_decimal(value / 10, decimal + 1);
-}
 // =====================================================
 
 // Testa vetor de 100 mil números totalmente ordenado decrescentemente
@@ -204,7 +199,7 @@ void test_decrescent_numbers()
 {
 	ARRAY *array = new_array(100000);
 	for (int index = 100000; index > 0; index--)
-		add(array, index / get_decimal(index, 1));
+		add(array, index);
 	merge_sort(array, 0, array->length);
 	// print_array(array); // Remover este comentário para ver a saída
 }
@@ -214,7 +209,7 @@ void test_crescent_numbers()
 {
 	ARRAY *array = new_array(100000);
 	for (int index = 1; index <= 100000; index++)
-		add(array, index / get_decimal(index, 1));
+		add(array, index);
 	merge_sort(array, 0, array->length);
 	// print_array(array); // Remover este comentário para ver a saída
 }
@@ -224,9 +219,9 @@ void test_crescent_decrescent_numbers()
 {
 	ARRAY *array = new_array(100000);
 	for (int index = 1; index <= 50000; index++)
-		add(array, index / get_decimal(index, 1));
+		add(array, index);
 	for (int index = 100000; index >= 50001; index--)
-		add(array, index / get_decimal(index, 1));
+		add(array, index);
 	merge_sort(array, 0, array->length);
 	// print_array(array); // Remover este comentário para ver a saída
 }
@@ -236,9 +231,9 @@ void test_decrescent_crescent_numbers()
 {
 	ARRAY *array = new_array(100000);
 	for (int index = 50000; index >= 1; index--)
-		add(array, index / get_decimal(index, 1));
+		add(array, index);
 	for (int index = 50001; index <= 100000; index++)
-		add(array, index / get_decimal(index, 1));
+		add(array, index);
 	merge_sort(array, 0, array->length);
 	// print_array(array); // Remover este comentário para ver a saída
 }
@@ -255,7 +250,7 @@ void test_random_numbers()
 		sorted_number = (sorted_number % (999999999 - 100000000)) + 100000000;
 
 		int number = sorted_number % 100001;
-		add(array, number / get_decimal(number, 1));
+		add(array, number);
 	}
 	merge_sort(array, 0, array->length);
 	// print_array(array) // Remover este comentário para ver a saída
